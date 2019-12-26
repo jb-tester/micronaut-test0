669fba24f2d64ff4989c8968886e5eb0c8a61789
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2019-12-26 12:58:23 +0300

    minor

--- src/main/java/hello/world/HomeController.java
+++ src/main/java/hello/world/HomeController.java
@@ -4,6 +4,7 @@ import hello.world.beansToInject.*;
 import hello.world.confProperties.MyProperties;
 import hello.world.confProperties.MyPropertiesExtended;
 import hello.world.qualifiers.SymbolQualifier;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
@@ -11,7 +12,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * *******************************
@@ -41,7 +41,8 @@ public class HomeController {
     @Inject @Named("${myprops.ext.boo}") // todo: no completion for bean names; no navigation to single matching bean
             Greeting greeting;
 
-    @Inject @SymbolQualifier // todo: qualified bean is not used as single candidate on navigation
+    @Inject
+    @SymbolQualifier // todo: qualified bean is not used as single candidate on navigation
             Punctuation symbol;
 
     // configuration properties:
@@ -50,8 +51,16 @@ public class HomeController {
     @Inject
     MyPropertiesExtended extProperties;
 
+    @Value("${myprops.ext.bar-prop:`vvv`}")
+    String vvv;
+
+    @Get("/guest/${myprops.bar-prop}")
+    public String bar() {
+        return decorator.getDecor() + greeting.getMessage() + ", user" + symbol.getSymbol() + decorator.getDecor();
+    }
+
     @Get("/world")
-    public String foo(){
+    public String foo() {
         System.out.println("foo list:");
         for (Foo foo : fooList) {
             System.out.println(foo.getId());
@@ -65,10 +74,7 @@ public class HomeController {
         System.out.println("custom config property usages:");
         System.out.println(myProperties.getFooProp());
         System.out.println(extProperties.getBoo());
+        System.out.println(this.vvv);
         return decorator.getDecor()+greeting.getMessage()+", world"+symbol.getSymbol()+decorator.getDecor();
-    }
-    @Get("/guest/${myprops.bar-prop}")
-    public String bar(){
-        return decorator.getDecor()+greeting.getMessage()+", user"+symbol.getSymbol()+decorator.getDecor();
     }
 }
